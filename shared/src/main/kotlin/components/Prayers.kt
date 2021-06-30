c83c5cf60d0b51d750380eef820a792574d336c1
Joshua de Wet <59136536+Dewera@users.noreply.github.com>
2021-06-30 19:21:49 +1200

    Extended prayer component

--- shared/src/main/kotlin/components/Prayers.kt
+++ shared/src/main/kotlin/components/Prayers.kt
@@ -1,12 +1,17 @@
 package shared.components
 
 import kraken.plugin.api.Actions
+import kraken.plugin.api.Client
 import shared.enums.Action
 import shared.enums.Curse
 import shared.enums.Prayer
 import shared.utilities.ComponentHash
 
-object Overhead {
+object Prayers {
+    fun isPrayerActive(): Boolean {
+        return Client.getConVarById(6890)?.value == 0
+    }
+
     fun toggleCurse(curse: Curse) {
         Actions.menu(Action.Widget.id, 1, curse.id, ComponentHash.computeHash(1458, 39), 1)
     }
